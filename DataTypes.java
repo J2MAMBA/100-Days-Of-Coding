e33b8dfaae08ab3c0613370531a12e27f37b06b1
Gaurav Shinde <gaurangshinde94@gmail.com>
2022-04-24 07:58:48 +0530

    Second Program Of Challege (DataTypes.java)

---
+++ DataTypes.java
@@ -0,0 +1,44 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        
+        // Different types of data types available in Java
+
+        // int type
+        //The int data type can have values from -231 to 231-1 (32-bit signed two's complement integer).
+        int var = 4250000;
+        System.out.println("Printing an Integer type of value "+var);
+
+        // boolean type
+        //The boolean data type has two possible values, either true or false.
+        boolean flag = true;
+        System.out.println("Printing an boolean type of value "+flag);
+
+        //float type
+        //The float data type is a single-precision 32-bit floating-point.
+        //Default value: 0.0 (0.0f)
+        float floatingValue = 45.5f;
+        System.out.println("Printing an Floating type of value "+floatingValue);
+
+        //double type
+        double doubleValue = 45.4;
+        System.out.println("Printing an Double type of value "+doubleValue);
+
+        //byte
+        byte byteValue = 23;
+        System.out.println("Printing an Byte type of value "+byteValue);
+
+        //short
+        short shortValue = 300;
+        System.out.println("Printing an Short type of value "+shortValue);
+
+        //long type
+
+        long longValue = 1234567890L;
+        System.out.println("Printing an Long type of value "+longValue);
+
+        //char type
+        char charValue = 'C';
+        System.out.println("Printing an Char type of value "+charValue);
+
+    }
+}
